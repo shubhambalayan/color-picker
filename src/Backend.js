2c30f5dcafb5fc6243f99998731c7bbb4a407df1
shubhambalayan <s2002kb@gmail.com>
2022-04-02 17:34:13 +0530

    frotend connected with backend

--- src/Backend.js
+++ src/Backend.js
@@ -1,2 +1,2 @@
-// export const API = ''
-export const API = 'http://localhost:8000/api/v1'
+export const API = 'https://color-picker-dhanraj.herokuapp.com/api/v1'
+// export const API = 'http://localhost:8000/api/v1'
